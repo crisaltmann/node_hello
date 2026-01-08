313a8dbb610e53cbc7c7827641ca44502a51459c
Cristiano Altmann <cristianoaltmann@MacBook-Pro-de-Cristiano.local>
2026-01-08 20:45:26 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+// Porta onde o servidor vai rodar
+const PORT = 3000;
+
+// Endpoint Hello World
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/hello', (req, res) => {
+  res.json({
+    message: 'Olá, mundo!',
+    status: 'ok'
+  });
+});
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
