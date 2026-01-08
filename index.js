f7890bf8cae5d0d046afa672720cd5a84d37d8c6
Cristiano Altmann <cristianoaltmann@MacBook-Pro-de-Cristiano.local>
2026-01-08 20:50:59 -0300

    Update index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // Porta onde o servidor vai rodar
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Endpoint Hello World
 app.get('/', (req, res) => {
